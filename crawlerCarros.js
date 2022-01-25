ab33faff5d6abf3f65f30d8cbcdec2bddd1f7e48
Carlos Alberto Ribas Junior <68855956+KaduHod@users.noreply.github.com>
2022-01-25 12:55:50 -0300

    Update crawlerCarros.js

--- crawlerCarros.js
+++ crawlerCarros.js
@@ -2,55 +2,6 @@ const puppeteer = require('puppeteer')
 const preparaURL = require('./preparaURL')
 const fs = require('fs');
 
-/* let arrAnosCarros = ()=>{// array com ids dos anos do carro
-  let i = 0
-  let anos = [1950]
-  while( i < 8 ){
-      
-      anos.push(anos[i]+5)
-      i++
-  }
-  while( i < 41 ){
-      
-      anos.push(anos[i]+1)
-      i++
-  }
-  anos.forEach(ano=>{
-      ano.toString()
-  })
-  return anos.toString().split(',')
-}
-
-function formataParametrosEmURL(urls){
-  return `${urls[0]}&${urls[1]}&${urls[2]}`
-}
-
-function formataNomeCarro(str){
-  let urlNome = ''
-  const hasUpper = (str) => /[A-Z]/.test(str);
-
-  if(hasUpper(str)){
-      str = str.split(/(?=[A-ZÀ-Ú])/)        
-      str[str.indexOf(' ')] = '%20'
-      str = `${str[0]}%20${str[1]}`
-  }
-  
-  urlNome = `q=${str}`
-  return urlNome
-}
-
-function formataAnoCarro(anoInicio, anoFinal){
-  let arrAnos = arrAnosCarros()
-  let urlAnos = `re=${arrAnos.indexOf(anoInicio)}&rs=${arrAnos.indexOf(anoFinal)}`
-  return urlAnos
-} 
-
-function formataPrecoCarro(precoInicial, precoFinal){
-  return `pe=${precoFinal}&ps=${precoInicial}`
-};
- */
-
-
 (async () => {
   const argumento = process.argv.slice()
 
